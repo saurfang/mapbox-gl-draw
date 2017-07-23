534229befe2875840ac36b062d7897a6be53f00a
Ryan Baumann <rsbaumann@gmail.com>
2017-07-23 09:43:04 -0700

    added initial files for draw-circle feature and modes

---
+++ src/modes/draw_circle.js
@@ -0,0 +1,33 @@
+const CommonSelectors = require('../lib/common_selectors');
+const doubleClickZoom = require('../lib/double_click_zoom');
+const Constants = require('../constants');
+const isEventAtCoordinates = require('../lib/is_event_at_coordinates');
+const createVertex = require('../lib/create_vertex');
+
+const DrawCircle = {};
+
+DrawCircle.onSetup = function() {
+  const circle = this.newFeature({
+    type: Constants.geojsonTypes.FEATURE,
+    properties: {},
+    geometry: {
+      type: Constants.geojsonTypes.POLYGON,
+      coordinates: [[]]
+    }
+  });
+
+  this.addFeature(circle);
+
+  this.clearSelectedFeatures();
+  doubleClickZoom.disable(this);
+  this.updateUIClasses({ mouse: Constants.cursors.ADD });
+  this.activateUIButton(Constants.types.POLYGON);
+  this.setActionableState({
+    trash: true
+  });
+
+  return {
+    circle,
+    currentVertexPosition: 0
+  };
+};
